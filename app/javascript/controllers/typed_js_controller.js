ea94df3ee62195dbbb50a1dd516dfb9aee801387
lux <luischn94@gmail.com>
2022-11-12 00:43:54 -0300

    version 35

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -5,10 +5,10 @@ export default class extends Controller {
   connect() {
     new Typed(this.element, {
       strings: [
-        `dog.dad`,
-        `bootcamp.teacher`,
-        `drone.pilot`,
-        `amateur.photographer`
+        `<button class="btn btn-outline-success btn-block">> <i class="fa-solid fa-shield-dog"></i> dog.dad</button>`,
+        `<button class="btn btn-outline-danger btn-block">> <i class="fa-solid fa-person-chalkboard"></i> bootcamp.teacher</button>`,
+        `<button class="btn btn-outline-primary btn-block">> <i class="fa-solid fa-splotch"></i> drone.pilot</button>`,
+        `<button class="btn btn-outline-info btn-block">> <i class="fa-solid fa-camera-retro"></i> amateur.photographer</button>`
       ],
       typeSpeed: 75,
       loop: true
