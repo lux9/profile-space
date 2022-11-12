62de23dba65a7bc1b6c42af8c1153cfe2c26d722
lux <luischn94@gmail.com>
2022-11-12 12:11:03 -0300

    collie

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -5,6 +5,7 @@ export default class extends Controller {
   connect() {
     new Typed(this.element, {
       strings: [
+        `<img src="assets/bc-before.jpg" class="bc-before" alt=""> <img src="assets/bc-after.jpg" class="bc" alt=""></img> <img src="assets/bc-before.jpg" class="bc-before" alt=""> <img src="assets/bc-after.jpg" class="bc" alt=""></img> <img src="assets/bc-before.jpg" class="bc-before" alt=""> <img src="assets/bc-after.jpg" class="bc" alt=""></img> `,
         `<button class="btn btn-outline-success btn-block">> <i class="fa-solid fa-shield-dog"></i> dog.dad</button>`,
         `<button class="btn btn-outline-danger btn-block">> <i class="fa-solid fa-person-chalkboard"></i> bootcamp.teacher</button>`,
         `<button class="btn btn-outline-primary btn-block">> <i class="fa-solid fa-splotch"></i> drone.pilot</button>`,
